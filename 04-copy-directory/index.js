4a4a6ddaa7d02f1cbcc3c097dfaa75def66e4790
PetrAlexkulakov <petr.alex.kulakov@gmail.com>
2022-11-02 12:16:22 +0300

    feat: 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,18 @@
+const fs= require('fs');
+const path = require('path');
 
+fs.mkdir('04-copy-directory/files-copy', (err)=>{
+    if(err) console.log(err.message)
+    else {
+        fs.readdir(path.join(__dirname,'./files'), (err,data) => {
+            data.forEach(file => {
+                let readStream =fs.createReadStream(path.join(__dirname,'./files',file))
+                let writeStream= fs.createWriteStream(path.join(__dirname,'/files-copy/'+file))
+        
+                readStream.on('data',(chunk)=>{
+                    writeStream.write(chunk)
+                })
+            })
+        })
+    }
+})
