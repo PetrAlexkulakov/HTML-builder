659d6947b04564aaa36ca96cb62047328d738644
PetrAlexkulakov <petr.alex.kulakov@gmail.com>
2022-11-09 21:22:05 +0300

    fis: in 4 delete folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,18 +1,20 @@
 const fs= require('fs');
 const path = require('path');
 
-fs.mkdir('04-copy-directory/files-copy', (err)=>{
-    if(err) console.log(err.message)
-    else {
-        fs.readdir(path.join(__dirname,'./files'), (err,data) => {
-            data.forEach(file => {
-                let readStream =fs.createReadStream(path.join(__dirname,'./files',file))
-                let writeStream= fs.createWriteStream(path.join(__dirname,'/files-copy/'+file))
-        
-                readStream.on('data',(chunk)=>{
-                    writeStream.write(chunk)
-                })
+
+async function removeAndMade(){
+    await fs.promises.rm(path.join(__dirname,'files-copy'),{force: true, recursive: true})  
+
+    await fs.promises.mkdir('04-copy-directory/files-copy')
+    fs.readdir(path.join(__dirname,'./files'), (err,data) => {
+        data.forEach(file => {
+            let readStream =fs.createReadStream(path.join(__dirname,'./files',file))
+            let writeStream= fs.createWriteStream(path.join(__dirname,'/files-copy/'+file))
+    
+            readStream.on('data',(chunk)=>{
+                writeStream.write(chunk)
             })
         })
-    }
-})
+    })
+}
+removeAndMade()
